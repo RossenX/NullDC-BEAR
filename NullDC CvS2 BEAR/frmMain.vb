99dcda1385a6908784bdc6ed4ef266f53234c249
RossenX <rossenx@gmail.com>
2021-04-21 03:05:36 +0300

    fixed diagonals

--- NullDC CvS2 BEAR/frmMain.vb
+++ NullDC CvS2 BEAR/frmMain.vb
@@ -6,12 +6,12 @@ Imports System.Text
 Imports System.Threading
 
 Public Class frmMain
-    Public IsBeta As Boolean = False
+    Public IsBeta As Boolean = True
 
     ' Update Stuff
     Dim UpdateCheckClient As New WebClient
 
-    Public Ver As String = "1.91a" 'Psst make sure to also change DreamcastGameOptimizations.txt
+    Public Ver As String = "1.91b" 'Psst make sure to also change DreamcastGameOptimizations.txt
 
     ' Public InputHandler As InputHandling
     Public NetworkHandler As NetworkHandling
